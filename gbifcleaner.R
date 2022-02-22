4c9fc6f9865583919c7227cd9cbea4d7387f85e5
Sander Devisscher <sander.devisscher@inbo.be>
2022-02-22 15:27:11 +0100

    first test
    #1

--- gbifcleaner.R
+++ gbifcleaner.R
@@ -9,15 +9,46 @@ gbifcleaner <- function(zipfile){
     stop(paste(zipfile, " does not exist"))
   }
   
-  # Free some RAM ####
+  ## Free some RAM ####
   fileinfo <- file.info(zipfile)
   
-  memory.limit(size = fileinfo$size/10000)
+  memory.limit(size = fileinfo$size/1000)
   gc(reset = TRUE)
   
+  ## Read data ####
   data <- read_tsv(unz(zipfile, "occurrence.txt"), 
                    col_types = c(decimalLatitude = col_number(),
                                  decimalLongitude = col_number(),
                                  establishmentMeans = col_character()))
   
+  ## Select required_columns ####
+  required_columns <- c("gbifID",
+                        "basisOfRecord",
+                        "taxonRank",
+                        "taxonomicStatus",
+                        "acceptedTaxonKey",
+                        "genus",
+                        "specificEpithet",
+                        "eventDate",
+                        "year",
+                        "month",
+                        "day",
+                        "decimalLatitude",
+                        "decimalLongitude",
+                        "coordinateUncertaintyInMeters",
+                        "occurrenceStatus",
+                        "countryCode")
+  
+  data_redux <- data %>% 
+    select(required_columns)
+  
+  ## rezip file ####
+  ### Export data ####
+  write_tsv(data_redux, "./data/data_redux.txt")
+  ### zipfile ####
+  zip(zipfile = zipfile,
+      files = "./data/data_redux.txt",
+      zip = Sys.getenv("R_ZIPCMD", "zip"))
+  ### Remove temp txt ####
+  file.remove("./data/data_redux.txt")
 }
